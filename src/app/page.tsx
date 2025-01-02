91f267245bc29c83fe39478a8c526a61c99b2b7d
Julien Marcesse <julien.marcesse@gmail.com>
2025-01-02 18:57:33 +0100

    fix: update LinkedIn URL to use the full domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-6">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://linkedin.com/julien-marcesse"
+          href="https://www.linkedin.com/in/julien-marcesse"
           target="_blank"
           rel="noopener noreferrer"
         >
